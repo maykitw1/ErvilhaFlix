f6286e7904b40f555f515de7c74bbb93821c96ee
maykitw1 <mayumiitikawa-ieg@instituto.ad>
2025-05-20 15:49:10 -0300

    primeiro commit

---
+++ semantica/muhehe.py
@@ -0,0 +1,6 @@
+print("--- Menu --- \n 1- Soma \n 2- Subtração \n 3- Multiplicação")
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite o segundo numero: "))
+opcao = int(input("Escolha: "))
+if opcao == 1:
+    print(n1+n2)
